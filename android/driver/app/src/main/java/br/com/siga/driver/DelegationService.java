bb356d517407411bac50b4dba8d94eef03088f5e
shopinoveloja-sys <seu-email@gmail.com>
2026-06-20 10:50:09 -0300

    Add Android TWA projects

---
+++ android/driver/app/src/main/java/br/com/siga/driver/DelegationService.java
@@ -0,0 +1,18 @@
+package br.com.siga.driver;
+
+
+import com.google.androidbrowserhelper.locationdelegation.LocationDelegationExtraCommandHandler;
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+            registerExtraCommandHandler(new LocationDelegationExtraCommandHandler());
+        
+    }
+}
+
